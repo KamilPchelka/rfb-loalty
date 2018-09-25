381c642784a2d526e9bbd753baac6e262ebfa7df
Kamil.Pchelka <kamil.pchelka@gmail.com>
2018-09-25 10:51:18 +0200

    Initial application generated by JHipster-5.3.4

---
+++ src/main/java/com/kamilpchelka/repository/PersistentTokenRepository.java
@@ -0,0 +1,19 @@
+package com.kamilpchelka.repository;
+
+import com.kamilpchelka.domain.PersistentToken;
+import com.kamilpchelka.domain.User;
+import java.time.LocalDate;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+/**
+ * Spring Data JPA repository for the PersistentToken entity.
+ */
+public interface PersistentTokenRepository extends JpaRepository<PersistentToken, String> {
+
+    List<PersistentToken> findByUser(User user);
+
+    List<PersistentToken> findByTokenDateBefore(LocalDate localDate);
+
+}
